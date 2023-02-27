a0ce3d39f1457cb7de61e5e4d44b6110f2725220
MarkKolyadich <70436486+Garmonik@users.noreply.github.com>
2023-02-27 05:38:39 +0300

    Delete 2.Numpy и линейная алгебра directory

--- 2.Numpy и линейная алгебра/№3/main.py
+++
@@ -1,9 +0,0 @@
-import numpy as np #не стирать!        
-
-def diag_2k(a):
-    b = np.diag(a)
-    sum = 0
-    for i in range(b.size):
-        if b[i] % 2 == 0:
-            sum += b[i]
-    return sum
--- 2.Numpy и линейная алгебра/№4/main.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-
-def cumsum(A):
-    result = np.cumsum(A, axis=-1) 
-    return result
